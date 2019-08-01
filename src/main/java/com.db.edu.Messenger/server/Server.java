1f956cb92a95c48425140b33e7e74c0483876569
Team AnyaAlisa <teamAnyaAlisa@gmail.com>
2019-08-01 11:49:30 +0300

    Create Maven Project

--- src/main/java/com.db.edu.Messenger/server/Server.java
+++ src/main/java/com.db.edu.Messenger/server/Server.java
@@ -1,4 +1,19 @@
 package com.db.edu.Messenger.server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    private static int port = 8080;
+    private static ServerSocket serverSocket;
+
+    public static void main(String[] args) {
+        try {
+            serverSocket = new ServerSocket(port);
+        } catch (IOException e) {
+            System.out.println("Can`t start server");
+        }
+
+    }
 }
